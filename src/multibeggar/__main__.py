67f0cbb1596477dab3fff7abe9f57aee19f4a82b
Aravind Pai <dragondive@outlook.in>
2023-08-13 10:09:36 +0530

    feat: add logger support using loguru

--- src/multibeggar/__main__.py
+++ src/multibeggar/__main__.py
@@ -1,5 +1,10 @@
+from loguru import logger
+from multibeggar.multibeggar import Multibeggar
+
+
 def entry_point():
-    print("Hello World")
+    logger.info("Hello World!")
+    mb = Multibeggar(complexity_tuning_factor=0.42)
 
 
 if __name__ == "__main__":
--- src/multibeggar/multibeggar.py
+++ src/multibeggar/multibeggar.py
@@ -1,3 +1,6 @@
+from loguru import logger
+
 class Multibeggar:
     def __init__(self, complexity_tuning_factor: float):
+        logger.debug("Multibeggar created!")
         self.complexity_tuning_factor = complexity_tuning_factor
