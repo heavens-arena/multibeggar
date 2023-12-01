85b7b432579de740e1ac4b0002568ca6bf8099c2
Aravind Pai <dragondive@outlook.in>
2023-12-01 12:20:01 +0530

    feat: add cli support with click

--- src/multibeggar/__main__.py
+++ src/multibeggar/__main__.py
@@ -1,10 +1,19 @@
 from loguru import logger
+import click
+
 from multibeggar.multibeggar import Multibeggar
 
 
-def entry_point():
+@click.command()
+@click.option(
+    "--complexity_tuning_factor",
+    default=0.01,
+    help="tuning factor for the multibeggar complexity algorithm",
+    show_default=True,
+)
+def entry_point(complexity_tuning_factor):
     logger.info("Hello World!")
-    mb = Multibeggar(complexity_tuning_factor=0.42)
+    mb = Multibeggar(complexity_tuning_factor=complexity_tuning_factor)
 
 
 if __name__ == "__main__":
--- src/multibeggar/multibeggar.py
+++ src/multibeggar/multibeggar.py
@@ -1,5 +1,6 @@
 from loguru import logger
 
+
 class Multibeggar:
     def __init__(self, complexity_tuning_factor: float):
         logger.debug("Multibeggar created!")
